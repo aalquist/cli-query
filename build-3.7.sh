25a965cc30dc4d5934d1aa3bfe7099e3502d865e
Aaron Alquist <aaalquis@akamai.com>
2019-08-21 23:39:39 -0400

    adding debug statements

--- build-3.7.sh
+++ build-3.7.sh
@@ -12,10 +12,15 @@ echo "buidling dockername: $DOCKERNAME"
 docker build -f python3.7.Dockerfile -t $DOCKERNAME:$commitId .
 
 echo "tagging dockername: $DOCKERNAME"
+
+echo "docker tag $DOCKERNAME:$commitId $DOCKERNAME:latest"
 docker tag $DOCKERNAME:$commitId $DOCKERNAME:latest 
+
+echo "docker tag $DOCKERNAME:$commitId $DOCKERNAME:v0.1"
 docker tag $DOCKERNAME:$commitId $DOCKERNAME:v0.1
+
+echo "docker tag $DOCKERNAME:$commitId $DOCKERNAME:py$PYTHON_Version"
 docker tag $DOCKERNAME:$commitId $DOCKERNAME:py$PYTHON_Version 
 
 echo "running docker run -v $(pwd):/cli-test --rm $DOCKERNAME python3 runtests.py"
-
 docker run -v $(pwd):/cli-test --rm $DOCKERNAME:$commitId python3 runtests.py
